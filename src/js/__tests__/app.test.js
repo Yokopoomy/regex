329a696778daa6330be7eedad8fc580d66a1f883
Evgeny Golovin <yokopoomy20@gmail.com>
2023-11-04 11:03:56 +0300

    Reworked the validation and tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,9 +1,15 @@
 import Validator from '../app';
 
-test('test name', () => {
-  const name = new Validator();
-	console.log(name)
-	const result = name.validateUsername('Yokop123-_oomy');
-
-  expect(result).toEqual('Имя подходит');
+test.each([
+  ['Y_okoE654Poo-my', true],
+  ['3yokopoomy', false],
+  ['Yoko@poomy', false],
+  ['Yoko1234poomy', false],
+  ['Yokopoomy_', false],
+  ['-Yokopoomy', false],
+  ['Yokopoomy3', false],
+])('Testing userName', (UserName, expectedValue) => {
+  const object = new Validator(UserName);
+  const result = object.validateUsername();
+  expect(result).toEqual(expectedValue);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,9 @@
 export default class Validator {
-	constructor() {
-		this.username;
-	}
+  constructor(username) {
+    this.username = username;
+  }
 
-	validateUsername(username) {
-		const reg1 = new RegExp('^[^-_\d][a-zA-Z-_0-9]*[^\-\_0-9]$');
-		const reg2 = new RegExp('(?<!\d)\d{1,3}(?!\d)');
-		if (reg1.test(username)) {
-			if (reg2.test(username)) {
-				return 'Имя подходит';;
-			}
-			return 'Имя содержит 4 и более цифр подряд';
-		}
-		return 'Имя содержит недопустимые символы';
- }
+  validateUsername() {
+    return this.username && /^[^-_\d][a-zA-Z-_0-9]*[^-_\d]$/.test(this.username) && !/\d{4,}/.test(this.username);
+  }
 }
